5e672483524138d97f09f96c1f1cbc5edb1ce503
NTer <NTer@MACHINE-LGU4SHC>
2022-04-23 21:54:31 +0300

    Добавьте файлы проекта.

---
+++ Linear equation root calculator.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, x;
+
+    cout << "This program calculates the root of the linear equation.\nThe linear equation has the form 'ax + b = 0'.\nEnter coefficients a and b:" << endl;
+
+    cout << "Coefficient a = ";
+    cin >> a;
+    
+    cout << "Coefficient b = ";
+    cin >> b;
+    
+    x = -b / a;
+    cout << "x = " << x << endl;
+}
